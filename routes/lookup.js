7b66a85570baa91b7c975e06794290c6749c4215
Raul Gutierrez <raul@shadow-moses.local>
2020-03-22 19:34:36 -0700

    cleaned up lookup api and functions, traced results to console

--- routes/lookup.js
+++ routes/lookup.js
@@ -6,11 +6,8 @@ const User = mongoose.model('User')
 
 module.exports = app => {
   async function FindUser(search, value) {
-    let user = await User.find({
-      [search]: value,
-    })
+    let user = await User.find({ [search]: value })
       .then(async userType => {
-        console.log(`USERTYPE: ${JSON.stringify(userType)}`)
         if (!userType.length) {
           return {
             error: `User ${value} does not exist`,
@@ -23,103 +20,81 @@ module.exports = app => {
           }
         }
 
-        const teacher = await FindTeacher(userType[0], userType[0].id).catch(e => {
-          console.log(`TEACHER ERROR RETURNED: ${e}`)
-          return e
-        })
+        const teacher = await FindTeacher(userType[0], userType[0].id).catch(e => e)
 
         return teacher
       })
-      .catch(e => {
-        console.log(`FINDUSER ERROR: ${e}`)
-
-        return e
-      })
+      .catch(e => e)
 
     return user
   }
 
   async function FindTeacher(user, value) {
-    Teacher.find({
-      userId: value,
-    }).then(x => console.log(`TEACHER NAME: ${x}`))
-
-    let result = await Teacher.find({
-      userId: value,
-    })
+    let result = await Teacher.find({ userId: value })
       .then(async teacher => {
         teacherObj = {
-          ...user._doc,
+          teacher: {
+            ...user._doc,
+            teacherId: teacher[0].id,
+            userId: teacher[0].userId,
+            schoolDistrict: teacher[0].schoolDistrict,
+          },
+          students: [],
         }
-
-        console.log(`TEACHER: ${teacher[0].students}`)
-        teacherObj.id = teacher[0].id
-        teacherObj.userId = teacher[0].userId
-        teacherObj.schoolDistrict = teacher[0].schoolDistrict
-        teacherObj.students = []
 
         for (let student of teacher[0].students) {
           await Student.find({ id: student })
             .then(student => {
-              console.log(`STUDENT: ${student}`)
-              teacherObj.students.push(student)
-              console.log(`TEACHEROBJ: ${JSON.stringify(teacherObj)}`)
+              teacherObj.students.push(student[0])
 
               return student
             })
-            .catch(e => {
-              console.log(`STUDENT ERROR: ${e}`)
-
-              return e
-            })
+            .catch(e => e)
         }
 
         return teacherObj
       })
-      .catch(e => {
-        console.log(`FINDTEACHER ERROR: ${e}`)
-
-        return e
-      })
+      .catch(e => e)
 
     return result
   }
 
-  // Find teacher by ID
+  // Find User by ID
   app.get('/User/id/:id', [check('id').isNumeric()], async (req, res) => {
+    // Check for parameter errors using express-validator
     const errors = validationResult(req)
 
-    console.log(`ERRORS: ${JSON.stringify(errors)}`)
-
+    // Return errors if any found
     if (!errors.isEmpty()) {
       res.send(errors)
     } else {
+      // Wait for user to be found and send promise back first
+      // Catch any errors and send back as response
       const teacher = await FindUser('id', req.params.id).catch(e => {
-        console.log(`ERROR RETURNED: ${e}`)
         res.send(e)
       })
 
-      console.log(teacher)
+      // Trace result to console
+      console.log(JSON.parse(JSON.stringify(teacher)))
 
+      // Fire promise and respond with result
       res.send(teacher)
     }
   })
 
-  // Find teacher by email
+  // Find User by email
+  // Same setup as above
   app.get('/User/email/:email', [check('email').isEmail()], async (req, res) => {
     const errors = validationResult(req.params.email)
-
-    console.log(`ERRORS: ${JSON.stringify(errors)}`)
 
     if (!errors.isEmpty()) {
       res.send(errors)
     } else {
       const teacher = await FindUser('email', req.params.email).catch(e => {
-        console.log(`ERROR RETURNED: ${e}`)
         res.send(e)
       })
 
-      console.log(teacher)
+      console.log(JSON.parse(JSON.stringify(teacher)))
 
       res.send(teacher)
     }
