b289bdb2b87da7c5247634646f84150cf5751105
Raul Gutierrez <rdgutierrez@gmail.com>
2020-03-23 18:42:43 -0700

    playing around with client in react

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+// const SearchNew = () => <h2>SearchNew</h2>
+// const Landing = () => <h2>Landing</h2>
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <h2 className='center' style={{ margin: '0' }}>
+            Teacher Lookup Tool
+          </h2>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header
